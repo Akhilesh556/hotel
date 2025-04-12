4719b1f3dd237cbd4ac92fd3f9097eb69288f678
Akhilesh556 <akhileshbindra1@gmail.com>
2025-04-12 21:38:21 +0530

    mongodb to online server

--- db.js
+++ db.js
@@ -1,6 +1,8 @@
 const mongoose= require("mongoose")
+require('dotenv').config()
 
-const mongoURL= 'mongodb://127.0.0.1:27017/hotels'
+// const mongoURL= process.env.mongodb_url_local
+const mongoURL= process.env.mongodb_url
 
 mongoose.connect(mongoURL,{
     
